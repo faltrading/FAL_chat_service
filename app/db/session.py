bbbcf16a7158073b68641da1795eb4689bcc0587
faltrading <faltrade@protonmail.com>
2026-02-23 17:21:13 +0100

    .

--- app/db/session.py
+++ app/db/session.py
@@ -1,6 +1,12 @@
+import ssl
+
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from app.core.config import settings
+
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
 
 engine = create_async_engine(
     settings.async_database_url,
@@ -8,6 +14,7 @@ engine = create_async_engine(
     max_overflow=10,
     pool_pre_ping=True,
     pool_recycle=300,
+    connect_args={"ssl": ssl_context},
 )
 
 async_session_factory = async_sessionmaker(
